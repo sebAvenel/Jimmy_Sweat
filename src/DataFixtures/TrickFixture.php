35530701630e8f973f6e4645d72a4fd0beea44de
Seb Avenel <sebastien.avenel@outlook.fr>
2019-07-16 14:12:21 +0200

    feat: finalization

--- src/DataFixtures/TrickFixture.php
+++ src/DataFixtures/TrickFixture.php
@@ -21,7 +21,7 @@ class TrickFixture extends Fixture implements DependentFixtureInterface
                 ->setCreatedAt($faker->dateTime('now', null))
                 ->setUpdatedAt($faker->dateTime('now', null))
                 ->setValidated(rand(0, 1))
-                ->setUser($this->getReference('user-' . rand(0, 99)))
+                ->setUser($this->getReference('user-' . rand(0, 101)))
                 ->setGroups(rand(1, 4))
                 ->setFirstImage(null);
             $manager->persist($trick);
